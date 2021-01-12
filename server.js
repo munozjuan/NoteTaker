bc5aca0f280e51e497e784cd1e5973f28c2f709e
munozjuan <munoz.juan07@hotmail.com>
2021-01-11 23:32:17 -0500

    changed setup

--- server.js
+++ server.js
@@ -8,3 +8,21 @@ const fs = require("fs");
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
+// Set up Express
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+// HTML Routes
+// Home Page Route
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+// Note Taker Route
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
